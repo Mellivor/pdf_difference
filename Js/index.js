9bc6f4cae603c759e624cf51ae20a283987c59bc
Dmytro Rybak <siagorat@gmail.com>
2023-07-12 00:31:40 +0300

    pdfjs first try

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,7 @@
+// const pdfjsLib = require("pdfjs-dist/legacy/build/pdf.js");
+console.log(pdfjsLib.getDocument('helloworld.pdf'));
+
+
 const opacity = document.querySelector("#opacity")
 const targets = document.querySelectorAll(".target");
 const firstDiv = document.querySelector(".first");
@@ -181,3 +185,32 @@ const comparation = () => {
 };
 
 compare.addEventListener("click", comparation)
+
+var loadingTask = pdfjsLib.getDocument('helloworld.pdf');
+loadingTask.promise.then(function (pdf) {
+    pdf.getPage(1).then(function (page) {
+        var scale = 1.5;
+        var viewport = page.getViewport({ scale: scale, });
+        // Support HiDPI-screens.
+        var outputScale = window.devicePixelRatio || 1;
+
+        var canvas = document.getElementById('the-canvas');
+        var context = canvas.getContext('2d');
+
+        canvas.width = Math.floor(viewport.width * outputScale);
+        canvas.height = Math.floor(viewport.height * outputScale);
+        canvas.style.width = Math.floor(viewport.width) + "px";
+        canvas.style.height = Math.floor(viewport.height) + "px";
+
+        var transform = outputScale !== 1
+            ? [outputScale, 0, 0, outputScale, 0, 0]
+            : null;
+
+        var renderContext = {
+            canvasContext: context,
+            transform: transform,
+            viewport: viewport
+        };
+        page.render(renderContext);
+    });
+});
